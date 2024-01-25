8b28623885c5c9d908b2dcfc662e4a25742faa16
Jasper Dansercoer <jasper@codictive.be>
2024-01-25 20:58:00 +0100

    Added link to pawn shop

--- web/src/components/Inventory.tsx
+++ web/src/components/Inventory.tsx
@@ -3,11 +3,17 @@ import { Divider, HStack, StyleProps, Text, VStack, Card } from "@chakra-ui/reac
 import React from "react";
 import { useDojoContext } from "@/dojo/hooks/useDojoContext";
 import { getDrugById } from "@/dojo/helpers";
-import { Bag } from "./icons";
+import { Bag, Home } from "./icons";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 export const Inventory = ({ ...props }: StyleProps) => {
+  const router = useRouter();
+  const { gameId } = router.query as { gameId: string };
   const { playerEntityStore } = useDojoContext();
   const { playerEntity } = playerEntityStore;
+
+  const open = true;
 
   return (
     <VStack {...props} w="full" align="flex-start" pb="0" gap={[0, "6px"]}>
@@ -24,36 +30,60 @@ export const Inventory = ({ ...props }: StyleProps) => {
         </HStack>
       </HStack>
 
-      <Card h="40px" px="20px" justify="center" w="full">
-        <HStack gap="10px" justify="center">
-          {playerEntity?.drugCount === 0 ? (
-            <Text color="neon.500">Your bag is empty</Text>
-          ) : (
-            playerEntity?.drugs.map((drug, key) => {
-              return (
-                drug.quantity > 0 && (
-                  <>
-                    <HStack gap="10px" key={`item-${key * 2}`}>
-                      <HStack color="yellow.400">
-                        {getDrugById(drug.id)?.icon({ boxSize: "26" })}
-                        <Text>{drug.quantity}</Text>
+      <HStack alignSelf="stretch" gap={4}>
+        {open ? (
+          <Link href={`/${gameId}/pawnshop`}>
+            <Card
+              h="40px"
+              w="120px"
+              alignItems="center"
+              flexDir="row"
+              gap={2}
+              bg="neon.800"
+              justifyContent="center"
+              color="neon.200"
+            >
+              <Home />
+              <Text>Open</Text>
+            </Card>
+          </Link>
+        ) : (
+          <Card h="40px" w="120px" alignItems="center" flexDir="row" gap={2} justifyContent="center" color="neon.500">
+            <Home />
+            <Text>Closed</Text>
+          </Card>
+        )}
+        <Card h="40px" px="20px" justify="center" flexGrow={1}>
+          <HStack gap="10px" justify="center">
+            {playerEntity?.drugCount === 0 ? (
+              <Text color="neon.500">Your bag is empty</Text>
+            ) : (
+              playerEntity?.drugs.map((drug, key) => {
+                return (
+                  drug.quantity > 0 && (
+                    <>
+                      <HStack gap="10px" key={`item-${key * 2}`}>
+                        <HStack color="yellow.400">
+                          {getDrugById(drug.id)?.icon({ boxSize: "26" })}
+                          <Text>{drug.quantity}</Text>
+                        </HStack>
                       </HStack>
-                    </HStack>
-                    <Divider
-                      key={`item-${key * 2 + 1}`}
-                      _last={{ display: "none" }}
-                      h="10px"
-                      orientation="vertical"
-                      borderWidth="1px"
-                      borderColor="neon.600"
-                    />
-                  </>
-                )
-              );
-            })
-          )}
-        </HStack>
-      </Card>
+                      <Divider
+                        key={`item-${key * 2 + 1}`}
+                        _last={{ display: "none" }}
+                        h="10px"
+                        orientation="vertical"
+                        borderWidth="1px"
+                        borderColor="neon.600"
+                      />
+                    </>
+                  )
+                );
+              })
+            )}
+          </HStack>
+        </Card>
+      </HStack>
     </VStack>
   );
 };
